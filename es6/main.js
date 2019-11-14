93bb049733c284677a5e78828e8cb138c3f55119
vik-mol <viktorvmoll@gmail.com>
2019-11-14 12:58:54 +0700

    Create form order

--- es6/main.js
+++ es6/main.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
     
+    const navbar = document.getElementById('nav-bar');
+    const header = document.getElementById('header')
+    const menuToggler = document.getElementById('header-toggle-btn');
+
+    menuToggler.addEventListener('click', function (e){
+        if(document.documentElement.clientWidth > 520) return;
+        navbar.classList.toggle('open');
+        this.classList.toggle('open');
+
+    });
+
+
+
+     
 
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  var navbar = document.getElementById('nav-bar');
+  var header = document.getElementById('header');
+  var menuToggler = document.getElementById('header-toggle-btn');
+  menuToggler.addEventListener('click', function (e) {
+    if (document.documentElement.clientWidth > 520) return;
+    navbar.classList.toggle('open');
+    this.classList.toggle('open');
+  });
+});
